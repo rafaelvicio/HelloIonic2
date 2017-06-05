bfc5cf9c23d391893436ab65478f91b481c18db1
Rafael Augusto Pacheco Baião Reis <rafaelvicio@icloud.com>
2017-06-05 09:22:18 -0300

    Aplicando actionSheet(Filtro por categoria)

--- src/pages/produtos/produtos.ts
+++ src/pages/produtos/produtos.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams, LoadingController, ToastController, AlertController } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, LoadingController, ToastController, AlertController, ActionSheetController } from 'ionic-angular';
 import { ProdutoServiceProvider } from '../../providers/produto-service/produto-service';
 import { PaginaBase } from '../../infraestrutura/PaginaBase';
 import { ProdutoModel } from '../../models/ProdutoModel';
@@ -21,10 +21,12 @@ export class ProdutosPage extends PaginaBase {
   produtos: ProdutoModel[];
   produtosFiltrados: ProdutoModel[];
   termoPesquisa: string;
+  filtradoPorCategoria: boolean;
+  categoriaSelecionada: string;
 
   constructor(public navCtrl: NavController, public navParams: NavParams,
   public loadingCtrl: LoadingController, public ToastCtrl: ToastController, public AlertCtrl: AlertController,
-  private produtoService: ProdutoServiceProvider) {
+  private produtoService: ProdutoServiceProvider, public ActionSheetCtrl: ActionSheetController) {
     super({ alertCtrl: AlertCtrl, loadingCtrl: loadingCtrl, toastCtrl: ToastCtrl})
     this.termoPesquisa = '';
   }
@@ -36,6 +38,8 @@ export class ProdutosPage extends PaginaBase {
       this.esconderLoading();
       this.produtos = response;
       this.produtosFiltrados = response;
+      this.filtradoPorCategoria = false;
+      this.categoriaSelecionada = '';
     },
     erro => {
       this.esconderLoading();
@@ -50,13 +54,68 @@ export class ProdutosPage extends PaginaBase {
   }
 
   filtrarProdutosPorNome(): void {
-    if(this.termoPesquisa == ''){
+    if(!this.filtradoPorCategoria){
+      if(this.termoPesquisa == ''){
       this.produtosFiltrados = this.produtos;
     } else {
       this.produtosFiltrados = this.produtos.filter((produto) => {
         return produto.nome.toLowerCase().indexOf(this.termoPesquisa.toLowerCase()) > -1;
       })
     }
+    } else {
+      if(this.termoPesquisa == ''){
+      this.produtosFiltrados = this.produtos.filter((produto) => {
+        produto.categoria == this.categoriaSelecionada;
+      });
+    } else {
+      this.produtosFiltrados = this.produtos.filter((produto) => {
+        return produto.nome.toLowerCase().indexOf(this.termoPesquisa.toLowerCase()) > -1 &&
+                produto.categoria == this.categoriaSelecionada;
+      })
+    }
+    }
+    
   }
+
+  filtrarPorCategoria(): void {
+    this.termoPesquisa = '';
+    let actionSheet = this.ActionSheetCtrl.create({
+      title: 'Filtro por categoria',
+      buttons: [
+        {
+          text: 'Doces',
+          handler: () => {
+            this.categoriaSelecionada == 'doce';
+            this.filtradoPorCategoria = true;
+            this.produtosFiltrados = this.produtos.filter((produto) => {
+              return produto.categoria = 'doce';
+            })
+          }
+        },
+        {
+          text: 'Salgado',
+          handler: () => {
+            this.categoriaSelecionada == 'salgado';
+            this.filtradoPorCategoria = true;
+            this.produtosFiltrados = this.produtos.filter((produto) => {
+              return produto.categoria = 'salgado';
+            })
+          }
+        },
+        {
+          text: '(Todas)',
+          role: 'cancel',
+          handler: () => {
+            this.categoriaSelecionada == '';
+            this.filtradoPorCategoria = false;
+            this.termoPesquisa = '';
+            this.produtosFiltrados = this.produtos
+          }
+        }
+      ]
+    });
+    actionSheet.present();
+  }
+
 
 }
